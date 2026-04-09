0eb283d58171ed7b6922bacf83a4823d18557558
nurdinalawiyah <nurdinahmada@gmail.com>
2026-04-09 15:55:32 +0700

    FEAT :: implement NetworkAudioStreamer to support remote URL audio playback in UriAudioSource

---
+++ just_audio/darwin/Classes/NetworkAudioStreamer.swift
@@ -0,0 +1,185 @@
+import Foundation
+import CoreAudio
+import AudioToolbox
+import AVFoundation
+
+protocol NetworkAudioStreamerDelegate: AnyObject {
+    func streamer(_ streamer: NetworkAudioStreamer, didDecodeBuffer buffer: AVAudioPCMBuffer)
+    func streamerDidFinishDecoding(_ streamer: NetworkAudioStreamer)
+    func streamer(_ streamer: NetworkAudioStreamer, didFailWithError error: Error)
+}
+
+class NetworkAudioStreamer: NSObject, URLSessionDataDelegate {
+    
+    weak var delegate: NetworkAudioStreamerDelegate?
+    let url: URL
+    
+    private var session: URLSession!
+    private var dataTask: URLSessionDataTask?
+    
+    private var streamID: AudioFileStreamID?
+    private var converter: AudioConverterRef?
+    private var inputFormat = AudioStreamBasicDescription()
+    private var isPlaying = false
+    
+    private var packetData = [Data]()
+    private var packetDescriptions = [AudioStreamPacketDescription]()
+    
+    private let outputFormat: AVAudioFormat
+    
+    init(url: URL) {
+        self.url = url
+        // Default output to standard PCM buffer format
+        self.outputFormat = AVAudioFormat(standardFormatWithSampleRate: 44100, channels: 2)!
+        super.init()
+        self.session = URLSession(configuration: .default, delegate: self, delegateQueue: DispatchQueue(label: "NetworkAudioStreamerQueue"))
+    }
+    
+    func start() {
+        dataTask = session.dataTask(with: url)
+        dataTask?.resume()
+        isPlaying = true
+    }
+    
+    func stop() {
+        dataTask?.cancel()
+        isPlaying = false
+        if let stream = streamID {
+            AudioFileStreamClose(stream)
+            streamID = nil
+        }
+        if let conv = converter {
+            AudioConverterDispose(conv)
+            converter = nil
+        }
+    }
+    
+    // MARK: - URLSessionDataDelegate
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        if streamID == nil {
+            let context = UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque())
+            AudioFileStreamOpen(context, propertyListenerProc, packetsProc, 0, &streamID)
+        }
+        
+        data.withUnsafeBytes { rawBuffer in
+            if let ptr = rawBuffer.baseAddress {
+                AudioFileStreamParseBytes(streamID!, UInt32(data.count), ptr, 0)
+            }
+        }
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        if let error = error {
+            delegate?.streamer(self, didFailWithError: error)
+        } else {
+            delegate?.streamerDidFinishDecoding(self)
+        }
+    }
+    
+    // MARK: - AudioFileStream Callbacks
+    
+    private let propertyListenerProc: AudioFileStream_PropertyListenerProc = { context, streamID, propertyID, flags in
+        let streamer = Unmanaged<NetworkAudioStreamer>.fromOpaque(context).takeUnretainedValue()
+        streamer.handlePropertyListener(streamID: streamID, propertyID: propertyID)
+    }
+    
+    private let packetsProc: AudioFileStream_PacketsProc = { context, byteCount, packetCount, data, packetDescriptions in
+        let streamer = Unmanaged<NetworkAudioStreamer>.fromOpaque(context).takeUnretainedValue()
+        streamer.handlePackets(byteCount: byteCount, packetCount: packetCount, data: data, packetDescriptions: packetDescriptions)
+    }
+    
+    private func handlePropertyListener(streamID: AudioFileStreamID, propertyID: AudioFileStreamPropertyID) {
+        if propertyID == kAudioFileStreamProperty_DataFormat {
+            var formatListSize: UInt32 = 0
+            AudioFileStreamGetPropertyInfo(streamID, kAudioFileStreamProperty_DataFormat, &formatListSize, nil)
+            var format = AudioStreamBasicDescription()
+            AudioFileStreamGetProperty(streamID, kAudioFileStreamProperty_DataFormat, &formatListSize, &format)
+            self.inputFormat = format
+            
+            var destFormat = outputFormat.streamDescription.pointee
+            AudioConverterNew(&self.inputFormat, &destFormat, &self.converter)
+        }
+    }
+    
+    // A simplified conversion using AudioConverter
+    private func handlePackets(byteCount: UInt32, packetCount: UInt32, data: UnsafeRawPointer, packetDescriptions: UnsafeMutablePointer<AudioStreamPacketDescription>?) {
+        guard let pDescriptions = packetDescriptions, let converter = self.converter else { return }
+        
+        let ptr = data.bindMemory(to: UInt8.self, capacity: Int(byteCount))
+        
+        for i in 0..<Int(packetCount) {
+            let desc = pDescriptions[i]
+            let packetData = Data(bytes: ptr.advanced(by: Int(desc.mStartOffset)), count: Int(desc.mDataByteSize))
+            self.packetData.append(packetData)
+            self.packetDescriptions.append(desc)
+        }
+        
+        // When we have enough packets, try to convert them
+        if self.packetData.count >= 20 { // Process in batches
+            convertPackets()
+        }
+    }
+    
+    private func convertPackets() {
+        guard let converter = self.converter else { return }
+        let packetCount = UInt32(packetData.count)
+        guard packetCount > 0 else { return }
+        
+        // Set up output buffer
+        let outputFrames: UInt32 = 4096
+        let pcmBuffer = AVAudioPCMBuffer(pcmFormat: outputFormat, frameCapacity: outputFrames)!
+        
+        var outputBufferList = AudioBufferList()
+        outputBufferList.mNumberBuffers = 1
+        outputBufferList.mBuffers.mNumberChannels = outputFormat.channelCount
+        outputBufferList.mBuffers.mDataByteSize = outputFrames * outputFormat.streamDescription.pointee.mBytesPerFrame
+        outputBufferList.mBuffers.mData = pcmBuffer.floatChannelData?.pointee
+
+        var framesToConvert = outputFrames
+        
+        var contextInfo = DecoderContext(streamer: self)
+        let status = AudioConverterFillComplexBuffer(converter, complexInputDataProc, &contextInfo, &framesToConvert, &outputBufferList, nil)
+        
+        if status == noErr && framesToConvert > 0 {
+            pcmBuffer.frameLength = framesToConvert
+            DispatchQueue.main.async {
+                self.delegate?.streamer(self, didDecodeBuffer: pcmBuffer)
+            }
+        }
+    }
+    
+    // MARK: - Decoder Context
+    
+    struct DecoderContext {
+        var streamer: NetworkAudioStreamer
+    }
+    
+    private let complexInputDataProc: AudioConverterComplexInputDataProc = { converter, ioNumberDataPackets, ioData, outDataPacketDescription, inUserData in
+        let context = inUserData?.bindMemory(to: DecoderContext.self, capacity: 1).pointee
+        guard let streamer = context?.streamer else {
+            ioNumberDataPackets.pointee = 0
+            return noErr
+        }
+        
+        if streamer.packetData.isEmpty {
+            ioNumberDataPackets.pointee = 0
+            return noErr
+        }
+        
+        let data = streamer.packetData.removeFirst()
+        let desc = streamer.packetDescriptions.removeFirst()
+        
+        ioData.pointee.mNumberBuffers = 1
+        ioData.pointee.mBuffers.mData = UnsafeMutableRawPointer(mutating: (data as NSData).bytes)
+        ioData.pointee.mBuffers.mDataByteSize = UInt32(data.count)
+        
+        if let outDesc = outDataPacketDescription {
+            outDesc.pointee = desc
+            outDesc.pointee.mStartOffset = 0
+        }
+        
+        ioNumberDataPackets.pointee = 1
+        return noErr
+    }
+}
--- just_audio/darwin/Classes/UriAudioSource.swift
+++ just_audio/darwin/Classes/UriAudioSource.swift
@@ -1,8 +1,15 @@
 
 
-class UriAudioSource: IndexedAudioSource {
+import Foundation
+import CoreMedia
+import AVFoundation
+
+class UriAudioSource: IndexedAudioSource, NetworkAudioStreamerDelegate {
     var url: URL
     var duration: CMTime = .invalid
+    var streamer: NetworkAudioStreamer?
+    var onPlayerLoaded: (() -> Void)?
+    weak var currentPlayerNode: AVAudioPlayerNode?
 
     init(sid: String, uri: String) {
         url = UriAudioSource.urlFrom(uri: uri)
@@ -11,24 +18,57 @@ class UriAudioSource: IndexedAudioSource {
     }
 
     override func load(engine _: AVAudioEngine, playerNode: AVAudioPlayerNode, speedControl _: AVAudioUnitVarispeed, position: CMTime?, completionHandler: @escaping () -> Void) throws {
-        let audioFile = try! AVAudioFile(forReading: url)
-        let audioFormat = audioFile.fileFormat
-
-        duration = UriAudioSource.durationFrom(audioFile: audioFile)
-        let sampleRate = audioFormat.sampleRate
-
-        if let position = position, position.seconds > 0 {
-            let framePosition = AVAudioFramePosition(sampleRate * position.seconds)
-
-            let missingTime = duration.seconds - position.seconds
-            let framesToPlay = AVAudioFrameCount(sampleRate * missingTime)
-
-            if framesToPlay > 1000 {
-                playerNode.scheduleSegment(audioFile, startingFrame: framePosition, frameCount: framesToPlay, at: nil, completionHandler: completionHandler)
-            }
+        self.onPlayerLoaded = completionHandler
+        self.currentPlayerNode = playerNode
+        
+        let scheme = url.scheme?.lowercased() ?? ""
+        if scheme == "http" || scheme == "https" {
+            streamer = NetworkAudioStreamer(url: url)
+            streamer?.delegate = self
+            streamer?.start()
         } else {
-            playerNode.scheduleFile(audioFile, at: nil, completionHandler: completionHandler)
+            let audioFile = try! AVAudioFile(forReading: url)
+            let audioFormat = audioFile.fileFormat
+
+            duration = UriAudioSource.durationFrom(audioFile: audioFile)
+            let sampleRate = audioFormat.sampleRate
+
+            if let position = position, position.seconds > 0 {
+                let framePosition = AVAudioFramePosition(sampleRate * position.seconds)
+
+                let missingTime = duration.seconds - position.seconds
+                let framesToPlay = AVAudioFrameCount(sampleRate * missingTime)
+
+                if framesToPlay > 1000 {
+                    playerNode.scheduleSegment(audioFile, startingFrame: framePosition, frameCount: framesToPlay, at: nil, completionHandler: completionHandler)
+                }
+            } else {
+                playerNode.scheduleFile(audioFile, at: nil, completionHandler: completionHandler)
+            }
         }
+    }
+    
+    // MARK: NetworkAudioStreamerDelegate
+    
+    func streamer(_ streamer: NetworkAudioStreamer, didDecodeBuffer buffer: AVAudioPCMBuffer) {
+        guard let playerNode = currentPlayerNode else { return }
+        playerNode.scheduleBuffer(buffer, completionHandler: nil)
+        
+        // If this is the first buffer, trigger the completion handler so player logic can proceed
+        if self.onPlayerLoaded != nil {
+            DispatchQueue.main.async {
+                self.onPlayerLoaded?()
+                self.onPlayerLoaded = nil
+            }
+        }
+    }
+    
+    func streamerDidFinishDecoding(_ streamer: NetworkAudioStreamer) {
+        // Nothing special. Buffer schedule handles the end.
+    }
+    
+    func streamer(_ streamer: NetworkAudioStreamer, didFailWithError error: Error) {
+        print("Streamer error: \(error)")
     }
 
     override func getDuration() -> CMTime {
